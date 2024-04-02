e30654e5cfa24927e8225e5b3afdfe19359ecbcf
yuva008 <pyuvarani.2011@gmail.com>
2024-04-02 16:41:27 +0200

    created functions with and without parameter

---
+++ script.js
@@ -0,0 +1,32 @@
+function function_with_nopara()
+{
+    console.log("Bernadette had a boat");
+}
+
+function_with_nopara();
+
+function function_with_one(vehicle)
+{
+console.log("Bernadetted has" +vehicle);
+}
+function_with_one("car");
+
+function function_with_two(balance,cost)
+{
+    new_balance=balance-cost
+
+    console.log("your new balance is:" + new_balance)
+}
+
+function_with_two(5000,1000);
+
+function circle(radious)
+{
+   var pi=Math.PI
+   var area=radious*radious*pi
+
+    console.log("area of circle is:" +area);
+
+}
+
+circle(5)
